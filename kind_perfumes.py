8f03ff5304458e94d696516483f56ad302f37e39
ahmed9024 <80919449+ahmed9024@users.noreply.github.com>
2025-08-20 23:41:19 +0300

    Add files via upload

---
+++ kind_perfumes.py
@@ -0,0 +1,15 @@
+kind_peefumes=input("do you nedd peefumes for man or woman :\n")
+if kind_peefumes==man:
+    man=int(input("what  a number perfumes you need"))
+elif man==1:
+     print('one millon')   
+elif man== 4:
+    print('Blue chanel')
+elif kind_peefumes ==woman:
+    woman=int(input("what  a number perfumes you need") )
+elif  woman==2:
+     print('one millon') 
+elif woman==3:
+     print('chloe')    
+else:
+    print('we dont have this number')    
